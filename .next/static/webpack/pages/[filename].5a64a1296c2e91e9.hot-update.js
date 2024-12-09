48fcc119c64fb748afb5b5b3842da9d3dd33de1a
Jzbonner <jarrett.bonner@gmail.com>
2024-12-09 00:35:15 -0500

    hugo rebuild - project directory cleanup

---
+++ .next/static/webpack/pages/[filename].5a64a1296c2e91e9.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/[filename]",{
+
+/***/ "./content/global/index.json":
+/*!***********************************!*\
+  !*** ./content/global/index.json ***!
+  \***********************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"header":{"icon":{"name":"BiCollapseAlt","color":"green","style":"float"},"name":"jzbonner","color":"default","nav":[{"href":"","label":"Home"},{"href":"about","label":"About"},{"href":"posts","label":"Blog"}]},"footer":{"color":"default","social":{"facebook":"/","twitter":"/","instagram":"/"}},"theme":{"color":"blue","font":"sans","darkMode":"system"}}');
+
+/***/ })
+
+});
